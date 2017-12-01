863f6e474ff0156102a5ac365afa4203c7fb8ce1
3dw1nM0535 <workockmoses@gmail.com>
2017-12-02 00:52:57 +0300

    added emit function callback for sending and updating DOM on new message

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,2 +1,24 @@
 //Make connection
 var socket = io.connect('http://localhost:4000');
+
+//Query DOM
+var message = document.getElementById('message');
+var output = document.getElementById('output');
+var handle = document.getElementById('handle');
+var send = document.getElementById('send');
+
+//Emit event
+send.addEventListener('click', function () {
+  socket.emit('chat', {
+    message: message.value,
+    handle: handle.value
+  });
+  message.value = '';
+  handle.value = '';
+});
+
+//Lsiten for event on client
+socket.on('chat', function (data) {
+  output.innerHTML += '<p><strong>' + data.handle + ':</strong>' + data.message + '</p>';
+});
+
