f17ab13b668f8d241ec1d1e3b1a5013fff80db4c
3dw1nM0535 <workockmoses@gmail.com>
2017-12-02 01:22:04 +0300

    added keystroke listener on client

--- public/js/chat.js
+++ public/js/chat.js
@@ -6,6 +6,7 @@ var message = document.getElementById('message');
 var output = document.getElementById('output');
 var handle = document.getElementById('handle');
 var send = document.getElementById('send');
+var feedback = document.getElementById('feedback');
 
 //Emit event
 send.addEventListener('click', function () {
@@ -17,8 +18,19 @@ send.addEventListener('click', function () {
   handle.value = '';
 });
 
-//Lsiten for event on client
+//Listen for keypress
+message.addEventListener('keypress', function () {
+  socket.emit('typing', handle.value);
+});
+
+//Listen for event on client
 socket.on('chat', function (data) {
+  feedback.innerHTML = '';
   output.innerHTML += '<p><strong>' + data.handle + ':</strong>' + data.message + '</p>';
+});
+
+//Update DOM when user is typing
+socket.on('typing', function (data) {
+  feedback.innerHTML = '<p><em>' + data + ' is typing a message...</em></p>';
 });
 
